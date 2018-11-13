220066d1c42b609852cc338a74ddbf38e02c72fe
Joshua Leger <legermjoshua@gmail.com>
2018-11-13 11:41:02 -0800

    Add get method for files

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var app = express();
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var fs = require('fs');
+
+var fileDir = "./files";
+if(!fs.existsSync(fileDir)) {
+    fs.mkdirSync(fileDir);
+}
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
@@ -13,3 +19,8 @@ app.use(methodOverride());
 
 app.listen(8080);
 console.log('App started on port 8080');
+
+app.get('/api/files', function(request, response) {
+    var files = fs.readdirSync(fileDir);
+    response.json(files);
+});

ef21dac510af26db83f999ac82832b7bd55e6ffd
Joshua Leger <legermjoshua@gmail.com>
2018-12-13 13:27:47 -0800

    Added some more filler and removed routing

--- file-center/src/app/app.component.ts
+++ file-center/src/app/app.component.ts
@@ -4,13 +4,17 @@ import { Model } from "./model";
 @Component({
     selector: 'file-center',
     templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
     title = 'file-center';
     model = new Model();
 
     getTitle() {
-        return this.model.title
+        return this.model.title;
+    }
+
+    getFiles() {
+        return this.model.files;
     }
 }

797a0192606ee1a9236a95fae1dfdbdcc6e7483c
Joshua Leger <legermjoshua@gmail.com>
2020-06-24 19:50:08 -0700

    Trying a different table library

--- jquery-ui/js/index.js
+++ jquery-ui/js/index.js
@@ -2,4 +2,32 @@ $(function() {
     $.get("/api/config", function(config) {
         $('#banner').text(config.banner);
     })
+
+    $('#files').DataTable({
+        "paging": false,
+        "info": false,
+        "order": [[ 1, "asc" ]],
+        "columns": [
+            {
+                "orderable": false, 
+                "searchable": false,
+                "width": "5%"
+            },
+            {
+                "orderable": true, 
+                "searchable": true,
+                "width": "55%"
+            },
+            {
+                "orderable": true, 
+                "searchable": false,
+                "width": "20%"
+            },
+            {
+                "orderable": false, 
+                "searchable": false,
+                "width": "20%"
+            }
+        ]
+    });
 });

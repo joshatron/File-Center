eba360a4007ddff0247afcf6e09f01a71b8ae0dd
Joshua Leger <legermjoshua@gmail.com>
2020-07-13 08:49:51 -0700

    Added file uploading, still need to reload files afterward

--- jquery-ui/public/js/index.js
+++ jquery-ui/public/js/index.js
@@ -16,10 +16,15 @@ function getPrettySize(size) {
     }
 }
 
+
 $(function() {
-    // Insert Banner
     $.get("/api/config", function(config) {
+        //Insert Banner
         $('#banner').text(config.banner);
+        //Show upload if allowed
+        if(!config.uploads) {
+            $('#upload').hide();
+        }
     });
 
     //Setup table headers
@@ -38,107 +43,114 @@ $(function() {
         window.open('/api/downloadZip?files=' + JSON.stringify(toDownload), '_blank');
     });
 
+
+    getFiles();
+
     //Insert table data
-    $.get("/api/files", function(files) {
-        let path = window.location.pathname;
-        if(path === "/") {
-            path = "";
-        } else {
-            path = path.substring(7);
-        }
-
-        path.split("/").forEach(function(folder) {
-            for(file of files) {
-                if(file.name === folder) {
-                    files = file.files;
-                    break;
-                }
-            }
-        });
-
-        if(path !== "") {
-            $('#files tbody').append(
-                '<tr id="dir-up">' +
-                    '<td></td>' +
-                    '<td>..</td>' + 
-                    '<td>' + path + '</td>' +
-                    '<td></td>' +
-                '</tr>'
-            );
-        }
-
-        $('#dir-up').click(function() {
-            let pathParts = path.split("/");
-            let pathAbove = path.substring(0, path.length - pathParts[pathParts.length - 2].length - 1);
-
-            window.location.replace(window.location.origin + "/files/" + pathAbove);
-        });
-
-        files.forEach(file => {
-            let row = '<tr>' + 
-                '<td><input type="checkbox" class="file-checkbox" value="' + file.name + '"></td>' +
-                '<td';
-            if(file.type === 'directory') {
-                row = row + ' class="dir">' + 
-                '<i class="fas fa-folder" style="padding-right: 0.25rem;"></i>';
+    function getFiles() {
+        $.get("/api/files", function(files) {
+            let path = window.location.pathname;
+            if(path === "/") {
+                path = "";
             } else {
-                row = row + '>';
+                path = path.substring(7);
             }
-            row = row + file.name + '</td>' + 
-                '<td>' + getPrettySize(file.size) + '</td>' +
-                '<td>' +
-                    '<a href="/api/download?file=' + file.name + '" download>' +
-                        '<button class="btn btn-outline-primary float-right">' + 
-                            '<i class="fas fa-download"></i> ' +
-                            '<span class="d-none d-md-inline">Download</span>' +
-                        '</button>' +
-                    '</a>' +
-                '</td>' +
-            '</tr>';
 
-            $('#files tbody').append(row);
-        });
-
-        $('.dir').click(function() {
-            window.location.replace(window.location.origin + "/files/" + path + $(this).text());
-        })
-
-        $('.file-checkbox').change(function() {
-            $('#head-checkbox').prop('checked', true);
-            $('.file-checkbox').each(function() {
-                if(!this.checked) {
-                    $('#head-checkbox').prop('checked', false);
+            path.split("/").forEach(function(folder) {
+                for(file of files) {
+                    if(file.name === folder) {
+                        files = file.files;
+                        break;
+                    }
                 }
             });
-        });
 
-        $('#files').DataTable({
-            "paging": false,
-            "info": false,
-            "autoWidth": false,
-            "order": [[ 1, "asc" ]],
-            "columns": [
-                {
-                    "orderable": false, 
-                    "searchable": false,
-                    "width": "5%"
-                },
-                {
-                    "orderable": true, 
-                    "searchable": true,
-                    "width": "55%"
-                },
-                {
-                    "orderable": false, 
-                    "searchable": false,
-                    "width": "20%"
-                },
-                {
-                    "orderable": false, 
-                    "searchable": false,
-                    "width": "20%"
+            $('#files tbody').empty();
+
+            if(path !== "") {
+                $('#files tbody').append(
+                    '<tr id="dir-up">' +
+                        '<td></td>' +
+                        '<td>..</td>' + 
+                        '<td>' + path + '</td>' +
+                        '<td></td>' +
+                    '</tr>'
+                );
+            }
+
+            $('#dir-up').click(function() {
+                let pathParts = path.split("/");
+                let pathAbove = path.substring(0, path.length - pathParts[pathParts.length - 2].length - 1);
+
+                window.location.replace(window.location.origin + "/files/" + pathAbove);
+            });
+
+            files.forEach(file => {
+                let row = '<tr>' + 
+                    '<td><input type="checkbox" class="file-checkbox" value="' + file.name + '"></td>' +
+                    '<td';
+                if(file.type === 'directory') {
+                    row = row + ' class="dir">' + 
+                    '<i class="fas fa-folder" style="padding-right: 0.25rem;"></i>';
+                } else {
+                    row = row + '>';
                 }
-            ]
+                row = row + file.name + '</td>' + 
+                    '<td>' + getPrettySize(file.size) + '</td>' +
+                    '<td>' +
+                        '<a href="/api/download?file=' + file.name + '" download>' +
+                            '<button class="btn btn-outline-primary float-right">' + 
+                                '<i class="fas fa-download"></i> ' +
+                                '<span class="d-none d-md-inline">Download</span>' +
+                            '</button>' +
+                        '</a>' +
+                    '</td>' +
+                '</tr>';
+
+                $('#files tbody').append(row);
+            });
+
+            $('.dir').click(function() {
+                window.location.replace(window.location.origin + "/files/" + path + $(this).text());
+            })
+
+            $('.file-checkbox').change(function() {
+                $('#head-checkbox').prop('checked', true);
+                $('.file-checkbox').each(function() {
+                    if(!this.checked) {
+                        $('#head-checkbox').prop('checked', false);
+                    }
+                });
+            });
+
+            $('#files').DataTable({
+                "paging": false,
+                "info": false,
+                "autoWidth": false,
+                "order": [[ 1, "asc" ]],
+                "columns": [
+                    {
+                        "orderable": false, 
+                        "searchable": false,
+                        "width": "5%"
+                    },
+                    {
+                        "orderable": true, 
+                        "searchable": true,
+                        "width": "55%"
+                    },
+                    {
+                        "orderable": false, 
+                        "searchable": false,
+                        "width": "20%"
+                    },
+                    {
+                        "orderable": false, 
+                        "searchable": false,
+                        "width": "20%"
+                    }
+                ]
+            });
         });
-    });
+    }
 });

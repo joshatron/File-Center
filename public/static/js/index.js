eff07e9d035e33b37b7eff4f7b7f9e76153c04f9
Joshua Leger <legermjoshua@gmail.com>
2020-07-19 11:28:47 -0700

    Password auth fully working now

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -27,72 +27,95 @@ $(function() {
         path = path.substring(7);
     }
 
+    let banner = "";
+    let uploads = false;
     let table = null;
 
-    //Setup table headers
-    $('#head-checkbox').prop('checked', false);
-    $('#head-checkbox').change(function() {
-        $('.file-checkbox').prop('checked', this.checked);
-    });
+    $.get("/api/config", function(config) {
+        banner = config.banner;
+        uploads = config.uploads;
 
-    $('#download-files').click(function() {
-        let toDownload = new Array();
-        $('.file-checkbox').each(function() {
-            if(this.checked) {
-                toDownload.push(path + $(this).val());
+        displayBanner();
+
+        $.ajax({
+            url: '/api/files',
+            type: 'GET',
+            success: function(response){
+                displayUploads();
+                getFiles();
+            },
+            error: function() {
+                displayAuth();
             }
         });
-        window.open('/api/downloadZip?files=' + JSON.stringify(toDownload), '_blank');
     });
 
-    //Check if need authentication
-    $.ajax({
-        url: '/api/files',
-        type: 'GET',
-        success: function(response){
-            applyConfig();
-            getFiles();
-        },
-        error: function() {
-            $.ajax({
-                url: '/authenticate',
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify({password: "password"}),
-                success: function(response){
-                    applyConfig();
-                    getFiles();
-                },
-                error: function(xhr, status, error) {
-                    console.log(error);
+    function displayBanner() {
+        $('#banner').text(banner);
+    }
+
+    function displayAuth() {
+        $('#auth').append(
+            '<div class="col form-group row mt-5">' +
+                '<label class="col-1 col-form-label" for="web-password">Password:</label>' +
+                '<input type="password" class="col-10 form-control" id="web-password">' +
+                '<button id="auth-submit" type="submit" class="col-1 btn btn-primary">Submit</button>' +
+            '</div>'
+        );
+
+        $('#auth-submit').click(function() {
+            submitAuth();
+        });
+
+        $('#web-password').bind("enterKey",function(e){
+            submitAuth();
+        });
+        $('#web-password').keyup(function(e){
+            if(e.keyCode == 13) {
+                $(this).trigger("enterKey");
+            }
+        });
+    }
+
+    function submitAuth() {
+        $.ajax({
+            url: '/authenticate',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({password: $('#web-password').val()}),
+            success: function(response){
+                hideAuth();
+                displayUploads();
+                getFiles();
+            },
+            error: function(xhr, status, error) {
+                $('#web-password').val('');
+            }
+        });
+    }
+
+    function hideAuth() {
+        $('#auth').empty();
+    }
+
+    function displayUploads() {
+        if(uploads) {
+            $('#dropzone').addClass("dropzone");
+            let url = "/api/upload";
+            if(path !== "") {
+                url = url + "/" + path;
+            }
+            var myDropzone = new Dropzone("#dropzone", { 
+                url: url,
+                maxFilesize: 1000000,
+                init: function(){
+                    this.on("complete", function(file) {
+                        this.removeFile(file);
+                        getFiles();
+                    });
                 }
             });
         }
-    });
-
-    function applyConfig() {
-        $.get("/api/config", function(config) {
-            //Insert Banner
-            $('#banner').text(config.banner);
-            //Show upload if allowed
-            if(config.uploads) {
-                $('#dropzone').addClass("dropzone");
-                let url = "/api/upload";
-                if(path !== "") {
-                    url = url + "/" + path;
-                }
-                var myDropzone = new Dropzone("#dropzone", { 
-                    url: url,
-                    maxFilesize: 1000000,
-                    init: function(){
-                        this.on("complete", function(file) {
-                            this.removeFile(file);
-                            getFiles();
-                        });
-                    }
-                });
-            }
-        });
     }
 
     //Insert table data
@@ -110,7 +133,43 @@ $(function() {
             if(table !== null) {
                 table.destroy();
             }
-            $('#files tbody').empty();
+            $('#file-table').empty();
+
+            $('#file-table').append(
+                '<table id="files" class="table table-striped table-sm">' +
+                    '<thead>' +
+                        '<tr>' +
+                            '<th><input type="checkbox" id="head-checkbox"></th>' +
+                            '<th>File</th>' +
+                            '<th>Size</th>' +
+                            '<th>' +
+                                '<button id="download-files" class="btn btn-outline-primary float-right" style="padding-right: 0.75rem;">' +
+                                    '<i class="fas fa-download" style="padding-right: 0rem;"></i>' +
+                                    '<span class="d-none d-md-inline" style="padding-right: 0rem;">Download</span> Selected' +
+                                '</button>' +
+                            '</th>' +
+                        '</tr>' +
+                    '</thead>' +
+                    '<tbody>' +
+                    '</tbody>' +
+                '</table>'
+            );
+
+            //Setup table headers
+            $('#head-checkbox').prop('checked', false);
+            $('#head-checkbox').change(function() {
+                $('.file-checkbox').prop('checked', this.checked);
+            });
+
+            $('#download-files').click(function() {
+                let toDownload = new Array();
+                $('.file-checkbox').each(function() {
+                    if(this.checked) {
+                        toDownload.push(path + $(this).val());
+                    }
+                });
+                window.open('/api/downloadZip?files=' + JSON.stringify(toDownload), '_blank');
+            });
 
             if(path !== "") {
                 $('#files tbody').append(

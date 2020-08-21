07a4166931f3f5daad5413eaaaab34eec1d7bfbb
Joshua Leger <legermjoshua@gmail.com>
2020-08-21 11:24:18 -0700

    Updated authentication so you can pass basic auth with every request instead of solely relying on cookies

--- server/authentication.js
+++ server/authentication.js
@@ -8,56 +8,83 @@ var webAccessToken;
 var adminPassword;
 var adminToken;
 
-exports.initialize = function(webAccessPass, adminPass) {
+function initialize(webAccessPass, adminPass) {
     this.updateWebAccessPassword(webAccessPass);
     this.updateAdminPassword(adminPass);
 }
 
-exports.getWebAccessToken = function(webAccessPass) {
-    if(webAccessPass === webAccessPassword) {
+function getWebAccessToken(webAccessPass) {
+    if(isWebAuthenticated(webAccessPass)) {
         return webAccessToken;
     }
 
     return "";
 }
 
-exports.getAdminToken = function(adminPass) {
-    if(adminPass === adminPassword) {
+function isWebAuthenticated(webPass) {
+    return webAccessPassword === webPass;
+}
+
+function getAdminToken(adminPass) {
+    if(isAdminAuthenticated(adminPass)) {
         return adminToken;
     }
 
     return "";
 }
 
-exports.checkAuthorized = function(request) {
+function isAdminAuthenticated(adminPass) {
+    return adminPassword === adminPass;
+}
+
+function checkAuthorized(request) {
     let path = request.baseUrl;
+    let password = request.header("Authorization");
+    if(password === undefined) {
+        password = "";
+    } else {
+        password = password.split(" ")[1];
+        password = Buffer.from(password, 'base64').toString('utf8');
+        password = password.split(":")[1];
+    }
 
     if(path.startsWith('/api/web') && webAccessPassword !== '') {
         return request.cookies['auth'] === adminToken || 
-               request.cookies['auth'] === webAccessToken;
+               request.cookies['auth'] === webAccessToken ||
+               isWebAuthenticated(password) || isAdminAuthenticated(password);
     } else if(path.startsWith('/api/admin')) {
-        return adminPassword !== "" && request.cookies['auth'] === adminToken;
+        return adminPassword !== "" && 
+        (request.cookies['auth'] === adminToken || isAdminAuthenticated(password));
     }
 
     return true;
 }
 
-exports.checkWebAuthenticated = function(request) {
+function checkWebAuthenticated(request) {
     return webAccessPassword === "" ||
            request.cookies['auth'] === adminToken || 
            request.cookies['auth'] === webAccessToken;
 }
 
-exports.checkAdminAuthenticated = function(request) {
+function checkAdminAuthenticated(request) {
     return request.cookies['auth'] === adminToken;
 }
 
-exports.updateWebAccessPassword = function(webAccessPass) {
+function updateWebAccessPassword(webAccessPass) {
     webAccessPassword = webAccessPass;
     webAccessToken = crypto.randomBytes(32).toString("hex");
 }
 
-exports.updateAdminPassword = function(adminPass) {
+function updateAdminPassword(adminPass) {
     adminPassword = adminPass;
     adminToken = crypto.randomBytes(32).toString("hex");
 }
+
+exports.initialize = initialize;
+exports.getWebAccessToken = getWebAccessToken;
+exports.getAdminToken = getAdminToken;
+exports.checkAuthorized = checkAuthorized;
+exports.checkWebAuthenticated = checkWebAuthenticated;
+exports.checkAdminAuthenticated = checkAdminAuthenticated;
+exports.updateWebAccessPassword = updateWebAccessPassword;
+exports.updateAdminPassword = updateAdminPassword;
